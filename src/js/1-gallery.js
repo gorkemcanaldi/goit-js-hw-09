4e1ed73c65f304929618b302e95bc8a48474ac0d
görkem can aldı <gorkem.aldi2003@gmail.com>
2025-06-13 12:47:33 +0300

    uzun uğraslar sonucu bitirdim

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,73 +1,98 @@
-// images array
-export const images = [
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Resim verileri
+const images = [
   {
-    small: './img/small1.jpg',
-    large: './img/large1.jpg',
-    description: 'Doğa 1',
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
   },
   {
-    small: './img/small2.jpg',
-    large: './img/large2.jpg',
-    description: 'Doğa 2',
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
   },
   {
-    small: './img/small3.jpg',
-    large: './img/large3.jpg',
-    description: 'Doğa 3',
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
   },
   {
-    small: './img/small4.jpg',
-    large: './img/large4.jpg',
-    description: 'Doğa 4',
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
   },
   {
-    small: './img/small5.jpg',
-    large: './img/large5.jpg',
-    description: 'Doğa 5',
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
   },
   {
-    small: './img/small6.jpg',
-    large: './img/large6.jpg',
-    description: 'Doğa 6',
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
   },
   {
-    small: './img/small7.jpg',
-    large: './img/large7.jpg',
-    description: 'Doğa 7',
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
   },
   {
-    small: './img/small8.jpg',
-    large: './img/large8.jpg',
-    description: 'Doğa 8',
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
   },
   {
-    small: './img/small9.jpg',
-    large: './img/large9.jpg',
-    description: 'Doğa 9',
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
   },
 ];
 
-// Galeri öğelerini oluşturma
-function createGalleryItems(items) {
-  return items
-    .map(
-      ({ small, large, description }) => `
+// Galeri elemanlarını oluşturma fonksiyonu
+function createGalleryItems(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
       <li class="gallery-item">
-        <a class="gallery-link" href="${large}" title="${description}">
-          <img class="gallery-image" src="${small}" alt="${description}" />
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            alt="${description}"
+          />
         </a>
       </li>
-    `
-    )
+    `;
+    })
     .join('');
 }
 
-// Galeriyi render et
-const galleryEl = document.querySelector('.gallery');
-galleryEl.innerHTML = createGalleryItems(images);
+// Galeriyi DOM'a ekleme
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = createGalleryItems(images);
 
-// SimpleLightbox başlat
+// SimpleLightbox başlatma
 const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'title', // title attribute'ten alır açıklamayı
-  captionDelay: 250, // 250 ms gecikme
+  captionsData: 'alt',
+  captionDelay: 250,
 });
