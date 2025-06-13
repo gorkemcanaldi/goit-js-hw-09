f3cbac9c01c5766de3ded1b8cb323123f1a729c0
görkem can aldı <gorkem.aldi2003@gmail.com>
2025-06-13 13:10:20 +0300

    ,

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-hw-09/', // BUNU EKLE!
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
